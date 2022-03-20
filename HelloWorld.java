0b3609dd86bb4b4016844205345d4741d4b394c8
Adarsh-cloud <53355633+Adarsh-cloud@users.noreply.github.com>
2022-03-20 13:21:11 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package proj.helloWorld;
+
+//Creation of public class
+
+public class HelloWorld {
+
+	// Creation of main method
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
